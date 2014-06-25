a553a4f2628c9733997398284065ca63833f7ed8
Marco Marche <marco.marche@gnucoop.com>
2014-06-25 17:38:09 +0200

    and some missing files...

---
+++ src/SelectUserLocationControl.js
@@ -0,0 +1,96 @@
+// Custom control - User can select a point to define his position
+// Fires a user-location-selected event on map
+
+var L = require('leaflet');
+
+L.Control.SelectUserLocation = L.Control.extend({
+    options: {
+        position: 'topleft',
+        icon: 'sulc-icon-location',
+        strings: {
+            title: "Set your current location"
+        }
+    },
+
+    onAdd: function (map) {
+        // control container
+        var container = L.DomUtil.create('div',
+            'leaflet-control-setuserlocation leaflet-bar leaflet-control');
+
+        // map reference
+        this._map = map;
+
+        // map pane reference
+        this._mapPane = map.getPanes()['mapPane'];
+
+        this._active = false;
+
+        // Construct the toggle link
+        this._link = L.DomUtil.create('a', 'leaflet-bar-part leaflet-bar-part-single ' + this.options.icon, container);
+        this._link.href = 'javascript:void(0);';
+        this._link.title = this.options.strings.title;
+
+        // bind link click event
+        L.DomEvent
+            .on(this._link, 'click', L.DomEvent.stopPropagation)
+            .on(this._link, 'click', L.DomEvent.preventDefault)
+            .on(this._link, 'click', function() {
+                this._toggleUserLocationSelection();
+            }, this)
+            .on(this._link, 'dblclick', L.DomEvent.stopPropagation)
+            .on(this._link, 'dblclick', L.DomEvent.preventDefault);
+
+        return container;
+    },
+
+    // activate / deactivate user's location selection
+    _toggleUserLocationSelection: function() {
+        this._active ? this._deactivateUserLocationSelection() : this._activateUserLocationSelection();
+    },
+
+    // deactivate user's location selection
+    _deactivateUserLocationSelection: function() {
+        this._map.off('click', this._userLocationSelected);
+
+        L.DomUtil.removeClass(this._link, 'sulc-icon-toggled');
+
+        L.DomUtil.removeClass(this._mapPane, 'sulc-custom-pointer');
+
+        this._active = false;
+    },
+
+    // activate user's location selection
+    _activateUserLocationSelection: function() {
+        this._map.on('click', this._userLocationSelected, this);
+
+        L.DomUtil.addClass(this._link, 'sulc-icon-toggled');
+
+        L.DomUtil.addClass(this._mapPane, 'sulc-custom-pointer');
+
+        this._active = true;
+    },
+
+    // when user select a location, fires an 'user-location-selected' event with selected
+    // latitude / longitude
+    _userLocationSelected: function(evt) {
+        L.DomEvent.stopPropagation(evt);
+        L.DomEvent.preventDefault(evt);
+
+        this._map.fire('user-location-selected', {
+            latlng: evt.latlng
+        });
+
+        this._deactivateUserLocationSelection();
+    }
+});
+
+L.Map.addInitHook(function () {
+    if (this.options.selectUserLocationControl) {
+        this.selectUserLocationControl = L.control.selectUserLocation();
+        this.addControl(this.selectUserLocationControl);
+    }
+});
+
+L.control.selectUserLocation = function (options) {
+    return new L.Control.SelectUserLocation(options);
+};
---
+++ src/UserLocation.js
@@ -0,0 +1,77 @@
+// Object that holds user location - adds a layer with user's location marker
+
+var L = require('leaflet'),
+    $ = require('jquery');
+
+function UserLocation(map, options) {
+    // options:
+    //   markerStyle - style for point - L.CircleMarker
+    //   markerRadius - point radius
+    //   circleStyle - style for circle - L.Circle
+    var options = $.extend({}, {
+        markerStyle: {
+            color: '#ff0000',
+            fillColor: '#ff0000',
+            fillOpacity: 1,
+            weight: 2,
+            opacity: 1
+        },
+        markerRadius: 5,
+        circleStyle: {
+            color: '#136AEC',
+            fillColor: '#136AEC',
+            fillOpacity: 0.15,
+            weight: 2,
+            opacity: 0.5
+        }
+    }, options || {});
+
+    this.map = map;
+    this._valid = false;
+    this._location = null;
+    this._marker = null;
+    this._circle = null;
+    this._layer = L.layerGroup();
+
+    map.addLayer(this._layer);
+
+    // add the marker/circle to the map or update their position if they exist
+    this._updateMarker = function() {
+        if(this._circle) {
+            this._circle.setLatLng(this._location);
+        }
+        else {
+            this._circle = L.circleMarker(this._location, options.circleStyle).addTo(this._layer);
+        }
+        if(this._marker) {
+            this._marker.setLatLng(this._location);
+        }
+        else {
+            this._marker = L.circle(this._location, options.markerRadius, options.markerStyle).addTo(this._layer);
+        }
+    };
+}
+
+// set user location - update marker
+// l - L.LatLng
+UserLocation.prototype.setLocation = function(l) {
+    if(!l instanceof L.LatLng) return;
+    this._location = L.latLng(l.lat, l.lng);
+    this._valid = true;
+    this._updateMarker();
+};
+
+// get user location
+// return L.LatLng
+UserLocation.prototype.getLocation = function() {
+    if(!this._valid) return null;
+
+    return L.latLng(this._location.lat, this._location.lng);
+};
+
+// return true if user location has been set (by gps or manual selection)
+UserLocation.prototype.isValid = function() {
+    return this._valid;
+};
+
+module.exports = UserLocation;
